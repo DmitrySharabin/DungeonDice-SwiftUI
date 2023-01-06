fb643bd1d9bc7149a49d714c3bd69b3aa76dc7d9
Dmitry Sharabin <dmitrysharabin@gmail.com>
2023-01-06 21:40:58 +0100

    Initial Commit

---
+++ DungeonDice/DungeonDiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  DungeonDiceApp.swift
+//  DungeonDice
+//
+//  Created by Dmitry Sharabin on 6.1.23..
+//
+
+import SwiftUI
+
+@main
+struct DungeonDiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
