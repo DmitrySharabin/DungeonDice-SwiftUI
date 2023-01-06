ea7138d07f6eeff6307b627ad51939d91407170b
Dmitry Sharabin <dmitrysharabin@gmail.com>
2023-01-06 21:56:21 +0100

    Ch. 4.1–4.4

--- DungeonDice/ContentView.swift
+++ DungeonDice/ContentView.swift
@@ -8,12 +8,48 @@
 import SwiftUI
 
 struct ContentView: View {
+    enum Dice: Int, CaseIterable {
+        case four = 4
+        case six = 6
+        case eight = 8
+        case ten = 10
+        case twelve = 12
+        case twenty = 20
+        case hundred = 100
+        
+        func roll() -> Int {
+            Int.random(in: 1...self.rawValue)
+        }
+    }
+    
+    @State private var resultMessage = ""
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Dungeon Dice")
+                .font(.largeTitle)
+                .fontWeight(.black)
+                .foregroundColor(.red)
+            
+            Spacer()
+            
+            Text(resultMessage)
+                .font(.largeTitle)
+                .fontWeight(.medium)
+                .multilineTextAlignment(.center)
+                .frame(height: 150)
+            
+            Spacer()
+            
+            LazyVGrid(columns: [GridItem(.adaptive(minimum: 102))]) {
+                ForEach(Dice.allCases, id: \.self) { dice in
+                    Button("\(dice.rawValue)-sided") {
+                        resultMessage = "You rolled a \(dice.roll()) on a \(dice.rawValue)-sided dice"
+                    }
+                }
+                .buttonStyle(.borderedProminent)
+            .tint(.red)
+            }
         }
         .padding()
     }
